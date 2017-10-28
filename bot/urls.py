92943729fac56e07a1cf88b06a575ab8bb27cc06
hashlash <muh.ashlah@gmail.com>
2017-10-28 21:52:54 +0700

    fix indentation

--- bot/urls.py
+++ bot/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls import url
 from .views import index
 
 urlpatterns = [
-  url(r'^$', index, name='index'),
+    url(r'^$', index, name='index'),
 ]
--- bot/views.py
+++ bot/views.py
@@ -12,30 +12,30 @@ parser = WebhookParser(settings.LINE_CHANNEL_SECRET)
 
 @csrf_exempt
 def index(request):
-  if request.method == 'POST':
-      signature = request.META['HTTP_X_LINE_SIGNATURE']
-      body = request.body.decode('utf-8')
+    if request.method == 'POST':
+        signature = request.META['HTTP_X_LINE_SIGNATURE']
+        body = request.body.decode('utf-8')
 
-      try:
-          events = parser.parse(body, signature)
-      except InvalidSignatureError:
-          return HttpResponseForbidden()
-      except LineBotApiError:
-          return HttpResponseBadRequest()
+        try:
+            events = parser.parse(body, signature)
+        except InvalidSignatureError:
+            return HttpResponseForbidden()
+        except LineBotApiError:
+            return HttpResponseBadRequest()
 
-      for event in events:
-          if isinstance(event, MessageEvent):
-              if isinstance(event.message, TextMessage):
-                  try:
-                      line_bot_api.reply_message(
-                          event.reply_token,
-                          TextSendMessage(text=event.message.text)
-                      )
-                  except LineBotApiError as e:
-                      print(e.status_code)
-                      print(e.error.message)
-                      print(e.error.details)
+        for event in events:
+            if isinstance(event, MessageEvent):
+                if isinstance(event.message, TextMessage):
+                    try:
+                        line_bot_api.reply_message(
+                            event.reply_token,
+                            TextSendMessage(text=event.message.text)
+                        )
+                    except LineBotApiError as e:
+                        print(e.status_code)
+                        print(e.error.message)
+                        print(e.error.details)
 
-      return HttpResponse()
-  else:
-      return HttpResponseBadRequest()
+        return HttpResponse()
+    else:
+        return HttpResponseBadRequest()
